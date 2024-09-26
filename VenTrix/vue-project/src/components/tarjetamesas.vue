1dc8cff89662a0682a880e569a01cbe7dc5353ff
jhonat1030 <jhonatansamuel2019@gmail.com>
2024-09-26 18:44:09 -0500

    combio #12

--- VenTrix/vue-project/src/components/tarjetamesas.vue
+++ VenTrix/vue-project/src/components/tarjetamesas.vue
@@ -1,47 +1,75 @@
 <template>
-    <v-btn class="mesas" color="green darken-1" large @click="navegarARuta('/cuerpo/seleccionarproductos')">
-      <img src="./icons/table-svgrepo-com (1).svg" alt="">
-        <span>Mesa 1</span>
+  <div class="mesas-container">
+    <v-btn
+      v-for="mesa in mesas"
+      :key="mesa.id"
+      class="mesas"
+      color="green darken-1"
+      large
+      @click="navegarARuta('/cuerpo/seleccionarproductos')"
+    >
+      <img src="./icons/table-svgrepo-com (1).svg" alt="Mesa Icon" />
+      <span>{{ mesa.nombre }}</span>
     </v-btn>
-  </template>
-  <script setup>
-import { useRouter } from 'vue-router'; 
+  </div>
+</template>
 
-const router = useRouter(); 
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const mesas = ref([]); 
+
 
 const navegarARuta = (ruta) => {
-  window.dispatchEvent(new Event('ocultarInicio')); 
+  window.dispatchEvent(new Event('ocultarInicio'));
   router.push(ruta);
 };
+
+
+const obtenerMesas = async () => {
+  try {
+    const response = await axios.get('http://localhost:8000/mesas'); 
+    mesas.value = response.data; 
+  } catch (error) {
+    console.error('Error al obtener las mesas:', error);
+  }
+};
+
+
+onMounted(() => {
+  obtenerMesas();
+});
 </script>
-  <style>
-    .mesas {
-    width: 100px ;
-    height: 100px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 20px;
-    background-color: #f0f0f0;
-    border: 5px solid var(--color_principal);
-    border-radius: 10px; 
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); 
-    cursor: pointer;
-    margin: 20px;
-    }
-  
-    .mesas img {
-      width: 100px; 
-      height: 100px;
-      
-    }
-  
-    .mesas h1 {
-      font-size: 20px;
-      font-weight:lighter;
-      font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-      text-align: center;
-      
-    }
+
+<style scoped>
+.mesas-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 20px; 
+}
+
+.mesas {
+  width: 100px;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  background-color: #f0f0f0;
+  border: 5px solid var(--color_principal);
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  margin: 20px;
+}
+
+.mesas img {
+  width: 100px;
+  height: 100px;
+}
 </style>
